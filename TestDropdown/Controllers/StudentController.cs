bedb8a23e8aa16802cc2de11d3ee13801808ae76
Rahul Sen <rsen253@gmail.com>
2016-08-26 01:40:55 +0530

    DynamivDropdown

---
+++ TestDropdown/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestDropdown.Models;
+
+namespace TestDropdown.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            Student obj = new Student();
+            List<Student> model = new List<Student>();
+            obj = new Student { StudentId = 1 , StudentName = "Batman" };
+            model.Add(obj);
+            obj = new Student { StudentId = 2 , StudentName = "Spiderman" };
+            model.Add(obj);
+            obj = new Student { StudentId = 3 , StudentName = "Superman" };
+            model.Add(obj);
+
+            var allStudent = (from m in model select m).ToList();
+            ViewBag.Student = new SelectList(allStudent, "StudentId" , "StudentName");
+
+            return View();
+        }
+    }
+}
